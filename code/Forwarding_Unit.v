feb9a2dfa5a75d5324a75ae9268623ec675c31e6
fsps60312 <fsps60312@yahoo.com.tw>
2018-11-28 20:13:34 +0800

    Forwarding_Unit v2

--- code/Forwarding_Unit.v
+++ code/Forwarding_Unit.v
@@ -1,10 +1,11 @@
-module Forwarding_Unit(Rs1_i,Rs2_i,Rdp_i,Rdpp_i,ForwardA_o,ForwardB_o);
-input[4:0]Rs1_i,Rs2_i,Rdp_i,Rdpp_i;
+module Forwarding_Unit(Rs1_i,Rs2_i,RegWrite_p_i,Rd_p_i,RegWrite_pp_i,Rd_pp_i,ForwardA_o,ForwardB_o);
+input[4:0]Rs1_i,Rs2_i,Rd_p_i,Rd_pp_i;
+input RegWrite_p_i,RegWrite_pp_i;
 output[1:0]ForwardA_o,ForwardB_o;
-assign ForwardA_o=(Rs1_i==Rdp_i)? 2'b10:
-				  (Rs1_i==Rdpp_i)? 2'b01:
+assign ForwardA_o=(RegWrite_p_i==1'b1 and Rs1_i==Rdp_i)? 2'b10:
+				  (RegWrite_pp_i==1'b1 and Rs1_i==Rdpp_i)? 2'b01:
 				  2'b00;
-assign ForwardB_o=(Rs2_i==Rdp_i)? 2'b10:
-				  (Rs2_i==Rdpp_i)? 2'b01:
+assign ForwardB_o=(RegWrite_p_i==1'b1 and Rs2_i==Rdp_i)? 2'b10:
+				  (RegWrite_pp_i==1'b1 and Rs2_i==Rdpp_i)? 2'b01:
 				  2'b00;
 endmodule
