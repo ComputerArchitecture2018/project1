6409fa90ecb2dff7ee2d766cab7171682fa2c700
karrieapple <karrierabbit@gmail.com>
2018-12-05 21:02:44 +0800

    detect Hazard

---
+++ code/Hazard_Detect.v
@@ -0,0 +1,14 @@
+module Hazard_Detect(
+	ID_EX_M,
+	write_addr,
+	read_addr,
+	IF_Op,
+	Pc_Op
+);
+input ID_EX_M;
+input [4:0] write_addr,read_addr;
+output IF_Op,Pc_Op;
+
+assign IF_Op = (ID_EX_M == 1 and write_addr == read_addr )?1:0;
+assign Pc_Op = (ID_EX_M == 1 and write_addr == read_addr )?1:0;
+endmodule
