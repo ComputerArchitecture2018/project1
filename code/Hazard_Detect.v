b9ae03292e4e6f9ecd08ad82274888d6023b3aba
karrieapple <karrierabbit@gmail.com>
2018-12-05 21:26:19 +0800

    modify

--- code/Hazard_Detect.v
+++ code/Hazard_Detect.v
@@ -3,12 +3,14 @@ module Hazard_Detect(
 	write_addr,
 	read_addr,
 	IF_Op,
-	Pc_Op
+	Pc_Op,
+	Mux_Op
 );
 input ID_EX_M;
 input [4:0] write_addr,read_addr;
-output IF_Op,Pc_Op;
+output IF_Op,Pc_Op,Mux_Op;
 
 assign IF_Op = (ID_EX_M == 1 and write_addr == read_addr )?1:0;
 assign Pc_Op = (ID_EX_M == 1 and write_addr == read_addr )?1:0;
+assign Mux_Op = (ID_EX_M == 1 and write_addr == read_addr )?1:0;
 endmodule
