7ded2c7e395cfb7f032ed8f808e04c01b3c74fc1
fsps60312 <fsps60312@yahoo.com.tw>
2018-12-05 18:47:31 +0800

    data memory

--- code/Data_Memory.v
+++ code/Data_Memory.v
@@ -1,16 +1,30 @@
 module Data_Memory
 (
-    addr_i, 
-    instr_o
+    addr_i,
+	data_i,
+	mem_write_i,
+	mem_read_i,
+	clk_i,
+	data_o,
 );
 
 // Interface
 input   [31:0]      addr_i;
-output  [31:0]      instr_o;
+input[31:0] data_i;
+input mem_write_i,mem_read_i,clk_i;
+output[31:0]data_o;
 
-// Instruction memory
+// Data memory
 reg     [31:0]     memory  [0:255];
 
-assign  instr_o = memory[addr_i>>2];  
+
+always @(posedge clk) begin
+	if(mem_write_i==1'b1)begin
+		memory[addr]<=data_i;
+	end
+	if(mem_read_i==1'b1)begin
+		data_o<=memory[addr];
+	end
+end
 
 endmodule
