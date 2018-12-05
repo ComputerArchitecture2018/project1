8068fc5e4053a46348d59ce9c374e222e4150600
fsps60312 <fsps60312@yahoo.com.tw>
2018-12-05 18:58:23 +0800

    update data memory

--- code/Data_Memory.v
+++ code/Data_Memory.v
@@ -1,29 +1,26 @@
 module Data_Memory
 (
+	clk_i,
     addr_i,
 	data_i,
 	mem_write_i,
-	mem_read_i,
-	clk_i,
 	data_o,
 );
 
 // Interface
 input   [31:0]      addr_i;
 input[31:0] data_i;
-input mem_write_i,mem_read_i,clk_i;
+input mem_write_i,clk_i;
 output[31:0]data_o;
 
 // Data memory
 reg     [31:0]     memory  [0:255];
 
+assign data_o=memory[addr_i];
 
-always @(posedge clk) begin
+always @(posedge clk_i) begin
 	if(mem_write_i==1'b1)begin
-		memory[addr]<=data_i;
-	end
-	if(mem_read_i==1'b1)begin
-		data_o<=memory[addr];
+		memory[addr_i]<=data_i;
 	end
 end
 
