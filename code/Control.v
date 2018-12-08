46ae5ebceab23946e11a3fcb8b3ad23066124de7
fsps60312 <fsps60312@yahoo.com.tw>
2018-12-09 01:27:03 +0800

    修正Control.v

--- code/Control.v
+++ code/Control.v
@@ -5,41 +5,43 @@ module Control(
 	Opcode,
 	EX,
 	Valid,
-	select,
+	select,//alu source. 0: rs2, 1: imm
 	PC_MUX_op
 );
 input Beq;
 input [31:0]inst;
-reg [2:0] func;
-reg [6:0] Op;
+wire [2:0] func;
+wire [6:0] Op;
 output flush_op,select;
 output [2:0]EX,Opcode;
 output Valid,PC_MUX_op;
 assign data_flush = (Beq == 1)?1:0;
 assign PC_MUX_op = (Beq == 1)?1:0;
-assign func = [14:12]inst;
-assign Op = [6:0]inst;
+assign func = inst[14:12];
+assign Op = inst[6:0];
 
-assign select = (inst[6] == 1)? 0://beq
-			(inst[5] == 0 and inst[4] == 1)? 1://addi 
-			(inst[5] == 0 and inst[4] == 0)? 1: //lw
-			(inst[5] == 1 and inst[4] == 0) ? 1://sw
-			(func == 3'b000 and inst[30] == 1)? 0://sub
-			(func == 3'b000 and inst[25] == 1)? 0://mul
-			(func == 3'b000)? 0://add
-			(func == 3'b111)? 0://and
-			(func == 3'b110)? 0;//or
+assign select = ({inst[6]} == 1'b1)? 1'b0://beq
+			(inst[5] == 1'b0 && inst[4] == 1'b1)? 1'b1://addi 
+			(inst[5] == 1'b0 && inst[4] == 1'b0)? 1'b1: //lw
+			(inst[5] == 1'b1 && inst[4] == 1'b0) ? 1'b1://sw
+			(func == 3'b000 && inst[30] == 1'b1)? 1'b0://sub
+			(func == 3'b000 && inst[25] == 1'b1)? 1'b0://mul
+			(func == 3'b000)? 1'b0://add
+			(func == 3'b111)? 1'b0://and
+			(func == 3'b110)? 1'b0://or
+			1'b1;
 assign EX = (inst[6] == 1)? 3'b110://beq
-			(inst[5] == 0 and inst[4] == 1)? 3'b011://addi 
-			(inst[5] == 0 and inst[4] == 0)? 3'b000: //lw
-			(inst[5] == 1 and inst[4] == 0) ? 3'b111://sw
-			(func == 3'b000 and inst[30] == 1)? 3'b100://sub
-			(func == 3'b000 and inst[25] == 1)? 3'b101://mul
+			(inst[5] == 0 && inst[4] == 1)? 3'b011://addi 
+			(inst[5] == 0 && inst[4] == 0)? 3'b000: //lw
+			(inst[5] == 1 && inst[4] == 0) ? 3'b111://sw
+			(func == 3'b000 && inst[30] == 1)? 3'b100://sub
+			(func == 3'b000 && inst[25] == 1)? 3'b101://mul
 			(func == 3'b000)? 3'b011://add
 			(func == 3'b111)? 3'b001://and
-			(func == 3'b110)? 3'b010;//or
-assign Opcode = [6:4]inst;
+			(func == 3'b110)? 3'b010://or
+			3'b000;
+assign Opcode = inst[6:4];
 
-assign Valid = (Op == 7'b0110011 or Op == 7'b0010011 or Op == 7'b0100011 or Op == 7'b0000011 or Op == 7'b1100011)? 1:0;
+assign Valid = (Op == 7'b0110011 || Op == 7'b0010011 || Op == 7'b0100011 || Op == 7'b0000011 || Op == 7'b1100011)? 1:0;
 
 endmodule
