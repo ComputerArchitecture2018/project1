ab6a5ffa19a1cb1d6c425b9f269ebe0f9221a032
fsps60312 <fsps60312@yahoo.com.tw>
2018-12-09 01:28:42 +0800

    修正Hazard_Detect

--- code/Hazard_Detect.v
+++ code/Hazard_Detect.v
@@ -10,7 +10,7 @@ input [2:0]ID_EX_M;
 input [4:0] reg1_addr,reg2_addr;
 output IF_Op,Pc_Op,Valid;
 
-assign IF_Op = (ID_EX_M == 3'b000 and reg1_addr == reg2_addr )?1:0;
-assign Pc_Op = (ID_EX_M == 3'b000 and reg1_addr == reg2_addr )?1:0;
-assign Valid = (ID_EX_M == 3'b000 and reg1_addr == reg2_addr )?0:1;
+assign IF_Op = (ID_EX_M == 3'b000 && reg1_addr == reg2_addr )?1:0;
+assign Pc_Op = (ID_EX_M == 3'b000 && reg1_addr == reg2_addr )?1:0;
+assign Valid = (ID_EX_M == 3'b000 && reg1_addr == reg2_addr )?0:1;
 endmodule
