5de79190d5e0031fe66044dbf6194bca5197d9dc
eugenechen-ourtimes <eugene.chen19971108@gmail.com>
2018-12-16 03:00:01 +0800

    ready to submit

--- code/Beq.v
+++ code/Beq.v
@@ -1,7 +1,7 @@
 module Beq(
 	data1_i,//from register
 	data2_i,//from register
-	Beq_Op, // if this is a beq instruction, I assume it's exist, but I'm not sure where did it came from
+	Beq_Op, // if this is a beq instruction; I assume it exists, but I'm not sure where did it came from
 	Beq //connect to the ID/EX
 );
 input [31:0] data1_i, data2_i;
--- code/Sign_Extend.v
+++ code/Sign_Extend.v
@@ -2,16 +2,4 @@ module Sign_Extend(data_i,data_o);
 input[11:0]data_i;
 output[31:0]data_o;
 assign data_o={{21{data_i[11]}},{data_i[10:0]}};
-/*always@(*)
-begin
-	if(data_i[31]==1'b0)
-	begin
-		out=data_i[31:20];
-	end
-	else
-	begin
-		out[31:11]=~21'b0;
-		out[10:0]=data_i[30:20];
-	end
-end*/
 endmodule
